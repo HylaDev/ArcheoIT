8f0c841d5f02a628199f6ddf7e2f01985d426b5c
HylaDev <ernestdossa.9@gmail.com>
2024-03-31 01:00:18 +0100

    first

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ArcheoIT - Accueil</title>
+    <!--Bootstrap 5 link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Mes feuilles de style-->
+    <link rel="stylesheet" href="styles/index.css">
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">LOGO</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mx-auto mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Qui sommes-nous?</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Nos activités
+                    </a>
+                <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">Stages</a></li>
+                    <li><a class="dropdown-item" href="#">Conférences</a></li>
+                    <li><a class="dropdown-item" href="#">Revues</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sites</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sites</a>
+                </li>
+            </ul>
+            <span class="navbar-text mr-auto">
+                <a href="" class="btn btn-primary">Contactez-nous</a>
+            </span>
+            </div>
+        </div>
+    </nav>
+
+    <section class='banner'>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text">
+                    <h1>
+                        L'archéologie au coeur de nos projets
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, quae? Est, laudantium quia! Necessitatibus, illo. Pariatur asperiores quo sit reprehenderit maiores 
+                        
+                    </p>
+                    <a href="" class="btn btn-primary ">Voir nos sites</a>
+                </div>
+                <div class="col-md-6">
+                    <img src="images/image.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--Bootstrap 5 script-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
